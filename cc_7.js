33e5ca66d45fdca55be837c7ff0164d9ed02ebab
Oliviagiles09 <giles11@usf.edu>
2025-02-16 21:28:05 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -15,3 +15,22 @@ function calculateHourlyWage(salary, hoursPerWeek) {
 
 consol.log(calculateHourlyWage(52000, 40));
 consol.log(calculateHourlyWage(75000, 35));
+
+// Task 3
+let calculateLoyaltyDiscount = (amount, years) => {
+  let discountRate
+  if (years >= 5) {
+    discountRate = 0.15;
+  }
+  else if (years >= 3) {
+    discountRate = 0.10;
+  }
+  else if (years < 3) {
+    discountRate = 0.05;
+  }
+  discountedPrice = amount - (amount * discountRate);
+  return 'Discounted Price: $${discountedPrice.toFixed(2)}';
+}
+
+consol.log(calculateLoyaltyDiscount(100, 6));
+consol.log(calculateloyaltyDiscount(200, 2));
