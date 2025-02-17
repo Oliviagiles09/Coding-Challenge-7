50e5966ccb2e01a9c67151cbf4d7f573f94fc54e
Oliviagiles09 <giles11@usf.edu>
2025-02-16 21:17:42 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,12 @@ function calculateInvoice(subtotal, taxRate, discount) {
 
 calculateInvoice(100, 0.08, 5);
 calculateInvoice(500, 0.1, 20);
+
+// Task 2 
+function calculateHourlyWage(salary, hoursPerWeek) {
+  const hourlyWage = salary / (hoursPerWeek * 52);
+  return 'Hourly Wage: $${hourlyWage.toFixed(2)}';
+}
+
+consol.log(calculateHourlyWage(52000, 40));
+consol.log(calculateHourlyWage(75000, 35));
